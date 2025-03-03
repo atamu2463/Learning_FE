5110d74cf8400708ab07d1698ea7e5a036cb9f32
asa-cording <asahiwork.cording@gmail.com>
2025-03-03 23:54:02 +0900

    make fizzbuzz

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.fizzbuzz.fizzbuzz;
 import com.example.model.Person;
 
 public class Main {
@@ -7,5 +8,9 @@ public class Main {
         //Personオブジェクトを作成
         Person person = new Person("山田太郎", 30);
         person.introduce();
+
+        int n = 100;
+        fizzbuzz fizzBuzzobject = new fizzbuzz();
+        fizzBuzzobject.fizzBuzz(n);
     }
 }
